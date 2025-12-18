4ff5c9247ad589fb6914dc51a46a2ea945997391
Chrystian Almeida <chrys481@gmail.com>
2025-12-18 16:22:57 -0300

    feat(expiration): add OTP-based token expiration with scheduler and boot reconciliation

--- lib/token_management_service/token_pool/expiration_scheduler.ex
+++ lib/token_management_service/token_pool/expiration_scheduler.ex
@@ -1,12 +1,62 @@
 defmodule TokenManagementService.TokenPool.ExpirationScheduler do
   use GenServer
 
-  def start_link(_args) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  alias TokenManagementService.TokenPool.TokenManager
+
+  @type token_id :: String.t()
+  @type timer_ref :: reference()
+
+  # Public API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, %{}, Keyword.put_new(opts, :name, __MODULE__))
+  end
+
+  @spec schedule(token_id(), non_neg_integer()) :: :ok
+  def schedule(token_id, ms) when is_binary(token_id) and is_integer(ms) and ms >= 0 do
+    GenServer.cast(__MODULE__, {:schedule, token_id, ms})
+  end
+
+  @spec cancel(token_id()) :: :ok
+  def cancel(token_id) when is_binary(token_id) do
+    GenServer.cast(__MODULE__, {:cancel, token_id})
+  end
+
+  # GenServer
+
+  @impl true
+  def init(_opts), do: {:ok, %{timers: %{}}}
+
+  @impl true
+  def handle_cast({:schedule, token_id, ms}, state) do
+    state = cancel_timer_if_exists(state, token_id)
+
+    ref = Process.send_after(self(), {:fire, token_id}, ms)
+    {:noreply, put_in(state.timers[token_id], ref)}
   end
 
   @impl true
-  def init(state) do
-    {:ok, state}
+  def handle_cast({:cancel, token_id}, state) do
+    {:noreply, cancel_timer_if_exists(state, token_id)}
+  end
+
+  @impl true
+  def handle_info({:fire, token_id}, state) do
+    state = update_in(state.timers, &Map.delete(&1, token_id))
+
+    send(TokenManager, {:expire_token, token_id})
+
+    {:noreply, state}
+  end
+
+  defp cancel_timer_if_exists(state, token_id) do
+    case Map.get(state.timers, token_id) do
+      nil ->
+        state
+
+      ref when is_reference(ref) ->
+        _ = Process.cancel_timer(ref)
+        update_in(state.timers, &Map.delete(&1, token_id))
+    end
   end
 end
--- lib/token_management_service/token_pool/token_manager.ex
+++ lib/token_management_service/token_pool/token_manager.ex
@@ -2,6 +2,9 @@ defmodule TokenManagementService.TokenPool.TokenManager do
   use GenServer
 
   alias TokenManagementService.Tokens.Repo, as: TokensRepo
+  alias TokenManagementService.TokenPool.ExpirationScheduler
+
+  @ttl_ms 2 * 60 * 1000
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, %{}, Keyword.put_new(opts, :name, __MODULE__))
@@ -17,13 +20,31 @@ defmodule TokenManagementService.TokenPool.TokenManager do
 
   @impl true
   def init(_opts) do
+    now = DateTime.utc_now()
+
     active_tokens =
       TokensRepo.list_active_tokens()
       |> Enum.reduce(%{}, fn token, acc ->
         Map.put(acc, token.id, token.last_activated_at)
       end)
 
-    {:ok, %{active: active_tokens}}
+    {active_after_reconcile, _scheduled_count} =
+      Enum.reduce(active_tokens, {%{}, 0}, fn {token_id, last_activated_at}, {acc, scheduled} ->
+        elapsed_ms = DateTime.diff(now, last_activated_at, :millisecond)
+        remaining_ms = @ttl_ms - elapsed_ms
+
+        cond do
+          remaining_ms <= 0 ->
+            _ = expire_token_if_active(token_id, now)
+            {acc, scheduled}
+
+          true ->
+            ExpirationScheduler.schedule(token_id, remaining_ms)
+            {Map.put(acc, token_id, last_activated_at), scheduled + 1}
+        end
+      end)
+
+    {:ok, %{active: active_after_reconcile}}
   end
 
   @impl true
@@ -38,8 +59,9 @@ defmodule TokenManagementService.TokenPool.TokenManager do
         token ->
           case TokensRepo.activate_token(token.id, now, %{reason: "allocated"}) do
             {:ok, _} ->
-              new_state =
-                put_in(state.active[token.id], now)
+              new_state = put_in(state.active[token.id], now)
+
+              ExpirationScheduler.schedule(token.id, @ttl_ms)
 
               {:reply, {:ok, token.id}, new_state}
 
@@ -48,17 +70,19 @@ defmodule TokenManagementService.TokenPool.TokenManager do
           end
       end
     else
-      # LRU eviction
       {lru_token_id, _} =
         Enum.min_by(state.active, fn {_id, last_used} -> last_used end)
 
       _ = TokensRepo.release_token(lru_token_id, now, %{reason: "lru_eviction"})
+      ExpirationScheduler.cancel(lru_token_id)
 
       case TokensRepo.activate_token(lru_token_id, now, %{reason: "allocated_after_eviction"}) do
         {:ok, _} ->
           new_state =
             state
             |> put_in([:active, lru_token_id], now)
+
+          ExpirationScheduler.schedule(lru_token_id, @ttl_ms)
 
           {:reply, {:ok, lru_token_id}, new_state}
 
@@ -67,6 +91,7 @@ defmodule TokenManagementService.TokenPool.TokenManager do
       end
     end
   end
+
   @impl true
   def handle_call({:release, token_id}, _from, state) do
     now = DateTime.utc_now()
@@ -78,12 +103,37 @@ defmodule TokenManagementService.TokenPool.TokenManager do
       true ->
         case TokensRepo.release_token(token_id, now, %{reason: "released_by_client"}) do
           {:ok, _} ->
+            ExpirationScheduler.cancel(token_id)
+
             new_state = update_in(state.active, &Map.delete(&1, token_id))
+
             {:reply, :ok, new_state}
 
           {:error, _step, reason, _} ->
             {:reply, {:error, reason}, state}
         end
     end
+  end
+
+  @impl true
+  def handle_info({:expire_token, token_id}, state) do
+    now = DateTime.utc_now()
+
+    if Map.has_key?(state.active, token_id) do
+      case TokensRepo.expire_token(token_id, now, %{reason: "ttl_expired"}) do
+        {:ok, _} ->
+          new_state = update_in(state.active, &Map.delete(&1, token_id))
+          {:noreply, new_state}
+
+        {:error, _step, _reason, _changes} ->
+          {:noreply, state}
+      end
+    else
+      {:noreply, state}
+    end
+  end
+
+  defp expire_token_if_active(token_id, now) do
+    TokensRepo.expire_token(token_id, now, %{reason: "ttl_boot_reconcile"})
   end
 end
